2662931e08cfff92af2c34064b48e391d4a616fb
user <duty_bound@aol.com>
2019-01-27 11:16:47 +0100

    Template

---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';	
+import ReactDOM from 'react-dom';
+
+const mountNode = document.getElementById('mountNode');
+const component = <h1>Hello World!</h1>;
+
+ReactDOM.render(component, mountNode);
